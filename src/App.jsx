1c336531c64c637a1a8c30540843f7317fd2b4b8
phai <zzmanhtienzz@gmail.com>
2022-05-20 11:11:05 +0700

    Adding dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React, {useState} from 'react'
+// import Accordion from "./components/Accordion";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items = [
+
+// const items = [
+//     {
+//         title: 'What is React?',
+//         content: 'React is a front end javascript framework'
+//     },
+//     {
+//         title: 'Why use React?',
+//         content: 'React is a front end JS library among engineers'
+//     },
+//     {
+//         title: 'How do you use React?',
+//         content: 'You use by creating components'
+//     }
+// ]
+//
+const options = [
     {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework'
+        label: 'The Color Red',
+        value: 'red'
+    }, {
+        label: ' The Color Green',
+        value: 'green'
     },
     {
-        title: 'Why use React?',
-        content: 'React is a front end JS library among engineers'
-    },
-    {
-        title : 'How do you use React?',
-        content: 'You use by creating components'
+        label: 'A Shade of Blue',
+        value: 'blue'
     }
-]
+];
 
 const App = () => {
+
+    const [selected, setSelected] = useState(options[0]);
+
     return (
-        <h1>
-            <Search/>
-        </h1>
+        <div>
+            <Dropdown options={options} selected={selected}
+                      onSelectedChange={setSelected}
+            />
+        </div>
     );
+
 }
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,36 +4,41 @@ import axios from "axios";
 const Search = () => {
 
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([]);
 
-    useEffect(
-        () => {
-            const search = async () => {
-                const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                    params: {
-                        action: 'query',
-                        list: 'search',
-                        origin: '*',
-                        format: 'json',
-                        srsearch: term
-                    }
-                });
-                setResults(data.query.search);
-            }
-            const timeoutId = setTimeout(() => {
-                if (term)
-                    search().then(er => {
-                        console.log(er)
-                    })
-            }, 500)
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+           setDebouncedTerm(term)
+        }, 1000);
 
-            return () => {
-                    clearTimeout(timeoutId);
-            }
-        }, [term]
-    )
+        return () => {
+            clearTimeout(timerId);
+        }
 
-    // dangerouslySetInnerHTML  => hacker my inject js in this cript
+    }, [term]);
+
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm
+                }
+            });
+            setResults(data.query.search);
+        };
+
+        search();
+
+    }, [debouncedTerm]);
+
+
+    // // dangerouslySetInnerHTML  => hacker my inject js in this cript
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
