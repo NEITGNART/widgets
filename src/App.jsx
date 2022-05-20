cbee7655bb57f7b2ae309a9e0566aad61a30a3d0
phai <zzmanhtienzz@gmail.com>
2022-05-20 18:26:24 +0700

    Adding Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import React, {useState} from 'react'
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
 
-// const items = [
-//     {
-//         title: 'What is React?',
-//         content: 'React is a front end javascript framework'
-//     },
-//     {
-//         title: 'Why use React?',
-//         content: 'React is a front end JS library among engineers'
-//     },
-//     {
-//         title: 'How do you use React?',
-//         content: 'You use by creating components'
-//     }
-// ]
-//
+// rsc for function
+
+// rccp for class
+
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a front end JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use by creating components'
+    }
+]
+
 const options = [
     {
         label: 'The Color Red',
@@ -38,9 +48,29 @@ const App = () => {
 
     const [selected, setSelected] = useState(options[0]);
 
+    // get path name using: window.location.pathname
+
+
     return (
         <div>
-            <Translate/>
+            <Header/>
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+
+            <Route path="/list">
+                <Search/>
+            </Route>
+
+            <Route path="/dropdown">
+                <Dropdown label = "Select a color" options = {options}
+                selected = {selected} onSelectedChange = {setSelected}/>
+            </Route>
+
+            <Route path="/translate">
+                <Translate items />
+            </Route>
+
         </div>
     );
 
